c09b91cadb33e0a405a3855f0f5290bb02e5fc73
pokemans123 <pranavarunkumar2010@gmail.com>
2026-06-09 20:24:33 -0400

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 			
 			system = "x86_64-linux";
 			modules = [ 
-				./configuration.nix
+				./hosts/qazniak/configuration.nix
 				home-manager.nixosModules.home-manager {
 					home-manager = {
 						useGlobalPkgs = true;
@@ -37,6 +37,30 @@
 
 		};
 
+		nixosConfigurations.qazniak-dell = nixpkgs.lib.nixosSystem {
+			
+			system = "x86_64-linux";
+			modules = [ 
+				./hosts/qazniak-dell/configuration.nix
+				home-manager.nixosModules.home-manager {
+					home-manager = {
+						useGlobalPkgs = true;
+						useUserPackages = true;
+						users.pranav = import ./home.nix;	
+						backupFileExtension = "backup";
+
+
+					};
+
+
+				}
+
+
+			];
+
+
+
+		};
 	};
 
 
