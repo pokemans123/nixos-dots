9c527f803c85b258760ebc543412edbfa7565c42
pokemans123 <pranavarunkumar2010@gmail.com>
2026-07-13 10:57:17 -0400

    Regular commit

---
+++ hosts/qazniak-dell/searx.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.searx = {
+    enable = true;
+
+    settings = {
+      server = {
+        port = 8888;
+        bind_address = "127.0.0.1";
+        secret_key = "";
+      };
+
+    };
+  };
+}
