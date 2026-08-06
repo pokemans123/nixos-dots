c4322fb7f8f156c8d2646b878873216bb0618a82
pokemans123 <pranavarunkumar2010@gmail.com>
2026-08-06 10:24:05 -0400

    improved searxng

--- modules/searx.nix
+++ modules/searx.nix
@@ -8,5 +8,23 @@
     settings.server = {
       bind_address = "0.0.0.0";
     };
+    settings.enabled_plugins = [
+      "Basic Calculator"
+      "Tor check plugin"
+      "Tracker URL remover"
+      "Open Acces DOI rewrite"
+    ];
+    settings.engines = lib.mapAttrsToList (name: value: { inherit name; } //value ){
+     "duckduckgo".disabled  = false;
+     "duckduckgo images".disabled  = false;
+     "brave".disabled  = false;
+     "github".disabled  = false;
+     "youtube".disabled  = false;
+     "ddg definitions".disabled = false;
+     "material icons".disabled  = false;
+     "currency".disabled = false;
+     "piped".disabled = false;
+     "wallhaven".disabled = false;
+    };
   };
 }
