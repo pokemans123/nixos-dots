5b52a6ac6a9555a5a5ac1ad8719a9e4695a70a62
pokemans123 <pranavarunkumar2010@gmail.com>
2026-08-06 09:19:59 -0400

    added searxng secret

--- modules/searx.nix
+++ modules/searx.nix
@@ -4,6 +4,7 @@
   services.searx = {
     enable = true;
     redisCreateLocally = true;
+    environmentFile = "/home/manager/.searxng.env";
     settings.server = {
       bind_address = "0.0.0.0";
     };
